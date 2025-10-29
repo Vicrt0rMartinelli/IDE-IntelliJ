d50ee848d7b22b32503b1dc82fcef219193b48a0
Vicrt0rMartinelli <josevictordasm17112006@gmail.com>
2025-10-28 22:57:21 -0300

    Teste 003

---
+++ src/app/Teste003.java
@@ -0,0 +1,25 @@
+package app;
+import java.util.Scanner;
+
+public class Teste003 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Double[] saldo = new Double[3];
+
+        for(int a=0; a<3;a++){
+            saldo[a] = scanner.nextDouble(); }
+
+        // TODO: Implemente a classificação do cliente com base no saldo:
+        for(int i = 0; i <3;i++){
+
+            if (saldo[i]<0){
+                System.out.println("Negativado");
+            }else if(saldo [i] >= 0 && saldo[i] <=500){
+                System.out.println("Baixo");
+            }else{
+                System.out.println("Confortavel");}
+        }
+        scanner.close();
+    }
+}
